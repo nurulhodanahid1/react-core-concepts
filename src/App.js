e8f6f1524e5b6ad89384c5f5021eb0b48d927899
nurulhodanahid11 <61221153+nurulhodanahid11@users.noreply.github.com>
2021-07-03 18:18:54 +0600

    Dynamacally data call in react test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-
+const player = ['David Warner', 'Tamim Iqbal', 'Martin Gubtil']
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Person name = "Steven Smith" jobs = "Batting"></Person>
+        <Person name = "Shakib Al Hasan" jobs = "all rounder"></Person>
+        <Person name = "Mohammad Amir" jobs = "Bowling"></Person>
+        <Person name = {player[0]} jobs = "This is come from an array"></Person>
       </header>
     </div>
   );
+}
+
+function Person(props) {
+  const personStyle = {
+    color:"#8E8CD8",
+    border:"2px solid #EA005E",
+    margin:"10px"
+  }
+  console.log(props);
+  return (
+    <div style={personStyle}>
+      <h2 style={{color:"#847545",backgroundColor:"#00B294"}}>Name: {props.name}</h2>
+      <p>No 1 {props.jobs} in the world</p>
+    </div>
+  )
 }
 
 export default App;
